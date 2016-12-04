ae30548707a18e9f199af84081f03cf792dce1a4
Mc <phenom.home@gmail.com>
2016-12-04 22:49:14 +0100

    Remove project.

--- wizards/remove.py
+++ wizards/remove.py
@@ -1,6 +1,25 @@
+from instruments.arguments import Arguments
+from instruments.file import File
 from wizards.__wizard import Wizard
 
 
 class Remove(Wizard):
     def call(self, args):
-        pass
+        config = self.assert_config()
+        assume = Arguments.assume(args, 1)
+        if config and assume:
+            alias = args[0]
+            data = File.read(
+                Wizard.Directory, Wizard.Config
+            )
+            projects = data.get('projects')
+            if alias in projects.keys():
+                projects.pop(alias)
+                File.write(
+                    Wizard.Directory, Wizard.Config,
+                    data=data
+                )
+            else:
+                pass
+        else:
+            pass
