eadac93215f6591391b1f1deff42c5c5cb7f080f
Mc <phenom.home@gmail.com>
2016-12-20 15:29:10 +0100

    Correct arguments for unix.

--- lib/instruments/git.py
+++ lib/instruments/git.py
@@ -5,7 +5,7 @@ class Git(object):
     @staticmethod
     def get_branch(directory):
         return subprocess.check_output(
-            'git -C {directory} rev-parse --abbrev-ref HEAD'.format(
+            'git -c {directory} rev-parse --abbrev-ref HEAD'.format(
                 directory=directory
             ), shell=True
         ).strip()
@@ -13,7 +13,7 @@ class Git(object):
     @staticmethod
     def get_changes(directory):
         return subprocess.check_output(
-            'git -C {directory} diff --shortstat'.format(
+            'git -c {directory} diff --shortstat'.format(
                 directory=directory
             ), shell=True
         ).strip()
